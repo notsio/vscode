a109af40859c30195dc85df86139587e55c1eb8d
point <alex.softx@gmail.com>
2019-02-18 19:19:13 +0200

    open note under cursor

--- extension.js
+++ extension.js
@@ -9,38 +9,35 @@ const workspace = vscode.workspace;
 const extensions = vscode.extensions;
 let gitApi = null;
 
-require('ssl-root-cas').inject();
+//require('ssl-root-cas').inject();
 const rp = require('request-promise');
-//require('https').globalAgent.options.ca = require('ssl-root-cas/latest').create();
-
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-//const vscode = require('vscode');
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 async function activate(context) {
+	let gutter_decorations = [];
+	let root_config = workspace.getConfiguration("nots-io");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "nots-io" is now active!');
+	let text_decoration = window.createTextEditorDecorationType({
+		light: {
+			backgroundColor: root_config.get("lightBgColor")
+		},
+		dark: {
+			backgroundColor: root_config.get("darkBgColor")
+		},
+		isWholeLine: true
+	});
+	let current_editor = null;
+	let show_nots = true;
+	let current_response = null;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	//let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-	//// The code you place here will be executed every time your command is executed
-
-	//// Display a message box to the user
-	//vscode.window.showInformationMessage('Hello World!');
-	//});
-
-	//context.subscriptions.push(disposable);
-
+	function get_keys() {
+		
+		let api_key = root_config.get("apiKey");
+		let project_api_key = root_config.get("projectApiKey");
+		return [api_key, project_api_key];
+	}
 
 	async function init_git() {
 		let gitExtension = extensions.getExtension('vscode.git');
@@ -65,50 +62,108 @@ async function activate(context) {
 	}
 
 	async function show_gutters() {
+		if (!window.activeTextEditor) {
+			return;
+		}
+		if(!show_nots) {
+			return;
+		}
+		current_editor = window.activeTextEditor;
 
 		let file_name = window.activeTextEditor.document.fileName;
 		let is_untitled = window.activeTextEditor.document.isUntitled;
 		let line_count = window.activeTextEditor.document.lineCount;
 		let repository = await get_repository();
+		let [api_key, project_api_key] = get_keys();
 
-		if (file_name && !is_untitled && line_count && repository) {
+
+		if (!api_key || !project_api_key) {
+			await window.showErrorMessage("Nots.io extension: Please set API keys on a workspace level");
+		}
+
+		if (file_name && !is_untitled && line_count && repository && api_key && project_api_key) {
 
 			let commit_sha = repository.getCommit("HEAD").hash;
-			let  options = {
+			let options = {
 				method: 'GET',
 				uri: 'https://nots.io/api/editor',
 				qs: {
-					api_key: "Q",
-					project_key: "W",
+					api_key: api_key,
+					project_key: project_api_key,
 					file_name: file_name,
 					commit_sha: commit_sha
 				},
-				headers:{
+				headers: {
 					'Content-Type': 'application/json'
 				},
 				strictSSL: false,
 				//json: true
 			}
-			console.log("11");
-			rp(options).then(r => { console.log(r);}).catch(console.log);
-			console.log("22");
+			response = await rp(options);
+			if(!response) {return;}
 
-			let decoration = window.createTextEditorDecorationType(
-				{
-					gutterIconPath: Uri.parse("https://nots.io/images/api_icon.png"),
-					gutterIconSize: "contain",
-				}
-			);
-			const range = new Range(new Position(0, 0), new Position(0, 0));
-			window.activeTextEditor.setDecorations(decoration, [range]);
+			current_response = JSON.parse(response);
+
+			let text_ranges = [];
+
+			for (let r of current_response) {
+				let decoration = window.createTextEditorDecorationType(
+					{
+						gutterIconPath: Uri.parse(r.icon_url),
+						gutterIconSize: "contain",
+					}
+				);
+				gutter_decorations.push(decoration);
+				const range = new Range(new Position(r.line_number, 0), new Position(r.line_number, 0));
+				window.activeTextEditor.setDecorations(decoration, [range]);
+	
+				text_ranges.push(new Range(new Position(r.line_number, 0), new Position(r.end_line_number, 0)));
+			}
+			window.activeTextEditor.setDecorations(text_decoration, text_ranges);
 		}
 	};
+
+	function remove_gutters(text_editor) {
+		text_editor.setDecorations(text_decoration, []);
+		
+		for(let gutter_decoration in gutter_decorations) {
+			text_editor.setDecorations(gutter_decoration, []);
+		}
+		gutter_decorations = [];
+	}
 	await show_gutters();
 
-	context.subscriptions.push(window.onDidChangeActiveTextEditor(async () => {
-		await show_gutters();
+	context.subscriptions.push(window.onDidChangeActiveTextEditor(async (event) => {
+		if (current_editor){
+			remove_gutters(current_editor);
+		}
+		if (window.activeTextEditor && event.document === window.activeTextEditor.document) {
+			await show_gutters();
+		}
 	}));
 
+	context.subscriptions.push(vscode.commands.registerCommand('extension.toggleNots', async () => {
+		if(show_nots) {
+			show_nots = false;
+			remove_gutters(window.activeTextEditor);
+		}
+		else {
+			show_nots = true;
+			show_gutters();
+		}
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('extension.openNots', async (event) => {
+		let selection = current_editor.selection.active;
+		if(selection && selection.line) {
+			let line = selection.line;
+			for(let r of current_response) {
+				if(r.line_number <= line && r.end_line_number >= line) {
+					vscode.env.openExternal(Uri.parse(r.url));
+				}
+			}
+		}
+	}));
 }
 exports.activate = activate;
 
